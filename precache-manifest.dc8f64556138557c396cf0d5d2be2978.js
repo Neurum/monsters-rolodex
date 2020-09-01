4681660f5e4e31fc494d193866626410b4a12fce
Curt House <66846507+Neurum@users.noreply.github.com>
2020-08-31 20:30:06 -0400

    Updates

---
+++ precache-manifest.dc8f64556138557c396cf0d5d2be2978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26fcc6a50f6ffa58ab05be554f28bd3e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cbe301ffaa65576aad51",
+    "url": "/monsters-rolodex/static/css/main.695babf7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe301ffaa65576aad51",
+    "url": "/monsters-rolodex/static/js/main.2cad65f0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
